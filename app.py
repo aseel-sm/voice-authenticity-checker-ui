5f6954a2950d7e777013d09fc55a80352c3e6a38
aseel-sm <aseelsm0027@gmail.com>
2024-04-14 19:24:53 +0530

    added model and styles

--- app.py
+++ app.py
@@ -1,41 +1,68 @@
 #Importing all the libraries needed
 import tensorflow as tf
-
-from keras import  preprocessing, Input
-#from multimodel baseline functions
-from keras.utils.vis_utils import  plot_model
-#import keras matrics
-
 import os
-import pandas as pd
-from nltk.corpus import stopwords
-# from nltk import word_tokenize
-from keras.preprocessing import image
-from keras.applications.vgg16 import  preprocess_input
-from keras.preprocessing.text import Tokenizer
 import numpy as np
-from keras.layers import  multiply
+import matplotlib
+matplotlib.use('Agg')
 from PIL import Image, ImageFile
-
 try:
     from PIL import Image
 except ImportError:
     import Image
 
 
-model_load =tf.keras.models.load_model(r'model_VGG.h5',compile=False)
+
+model_load =tf.keras.models.load_model(r'model_best.h5',compile=False)
 i="0SvkQMd.png"
 # Flask utils
 from flask import Flask,  request, render_template
 from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
-
+from pydub import AudioSegment
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+from scipy.signal import butter, filtfilt, spectrogram
+import cv2
 
 
 
 
 # Define a flask app
-app = Flask(__name__)
+app = Flask(__name__,static_folder='static',)
+
+
+
+freq_range = (1, 100)  # Specify the range of frequencies to keep
+
+# Define the duration of the spectrogram in seconds
+duration = 1.5
+
+def convert_to_wav(file_path):
+    wav_file_path = os.path.splitext(file_path)[0] + '.wav'
+    audio = AudioSegment.from_file(file_path)
+    audio.export(wav_file_path, format='wav')
+    return wav_file_path
+
+def save_img(filename):
+    fs, audio = wavfile.read(filename)
+
+    # Compute the number of frames for the specified duration
+    n_frames = int(duration * fs)
+    # Compute the spectrogram of the filtered audio signal
+    f, t, Sxx = spectrogram(audio[:n_frames], fs=fs) 
+    # Plot the spectrogram
+    plt.pcolormesh(t, f, 10*np.log10(Sxx), cmap='jet')
+    plt.axis('off')
+
+    output_filename = r'C:\Users\hisha\OneDrive\Desktop\VADbackup\VAD\static\images\test.png'
+    plt.savefig(output_filename, bbox_inches='tight', pad_inches=0)
+
+    # Clear the current plot for the next spectrogram
+    plt.clf()
+    img_arr=cv2.imread(output_filename)
+    img_arr=cv2.resize(img_arr,(369,496, ))
+    return img_arr
+
+
 
 
 @app.route('/', methods=['GET'])
@@ -43,56 +70,44 @@ def index():
     # Main page
     return render_template('index.html')
 
-def model_predict(img_path, model):
-    img = image.load_img(img_path, grayscale=False, target_size=(128, 128,3))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = np.array(x, 'float32')
-    x /= 255
-    preds = model.predict(x)
-    print("prediction:",preds)
-    a = preds
-    
-    ind=np.argmax(a)     
-    return ind
 
+    
+
+print("Hello")
 @app.route('/predict', methods=['GET','POST'])
 def upload():
+   
     if request.method == 'POST':
-    
+      
         # Get the file from post request
         print(request.files)
         f = request.files['image']
-      
+       
         # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
-        print("Base--",basepath)
+
         file_path = os.path.join(
             basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
-        print("Fileee----------------------------------")
+        
+        if not file_path.lower().endswith('.wav'):
+            file_path = convert_to_wav(file_path)
+            
         print("File----",file_path)
-        img = image.load_img(file_path, grayscale=False, target_size=(128, 128))
-        x = image.img_to_array(img)
-        print(x)
-        x = np.expand_dims(x, axis=0)
-        x = np.array(x, 'float32')
-
-        print(x.shape)
-        # x /= 255
-        # result=model_predict(file_path,model_load)
-        preds=model_load.predict(x)
-        print("prediction",preds)
-        # print(result)
-        print("Res",preds[0])
-        pred=preds[0][0]
-        print(pred)
+        # img = image.load_img(file_path, grayscale=False, target_size=(128, 128))
+        img=save_img(file_path)
+        input_data = np.expand_dims(img, axis=0)
+        preds=model_load.predict(input_data/255,verbose=False)
         res={}
-        if(preds==0):
-            res['status']=0
-        else:
+        if preds[0]<0.5:
+            print("Genuine")
             res['status']=1
-        return res
+        else:
+            print("Spoof")
+            res['status']=0
+        print("prediction",preds)
 
+        return res
+    return "HI"
 if __name__ == '__main__':
    app.run(debug=True,port=5001)
